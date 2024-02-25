a2afd2827b3bfd2a101e33d2fb468f7c9d2a55e9
Edrick Koda <z5349845@unsw.edu.au>
2024-02-25 16:05:13 +1100

    [wip] added header for home

---
+++ frontend/src/components/HomeHeader.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <p className="my-3">DevSoc presents</p>
+      <p className="text-blue-500 text-6xl font-extrabold">unilectives</p>
+      <p className="my-3 font-extrabold">
+        Your one-stop shop for UNSW course and elective reviews.
+      </p>
+    </div>
+  );
+}
